836a10a7ae659ff0fef5b12b6520137f021519b2
Tomas Sobota <t.sobota@volny.cz>
2023-01-29 12:37:08 +0100

    first version, basic layout, DB EF Migration, class Article, load image to server should be work

---
+++ OldGamerCry_ASP_Blog/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OldGamerCry_ASP_Blog.Data;
+using OldGamerCry_ASP_Blog.Models;
+using System.Diagnostics;
+
+namespace OldGamerCry_ASP_Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Article.ToListAsync());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OldGamerCry_ASP_Blog/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using pokusoblog2.Models;
+
+namespace OldGamerCry_ASP_Blog.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<pokusoblog2.Models.Article> Article { get; set; }
+    }
+}
